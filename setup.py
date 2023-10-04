4aa99c385623fba973ccdf127bfafc9855cbf1e8
Alan Liddell <aliddell@chanzuckerberg.com>
2023-10-04 16:08:21 -0400

    add napari to install_requires

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ package_name = "napari_ros"
 setup(
     name=package_name,
     version="0.1.0",
-    packages=[package_name],
+    packages=[package_name, "napari"],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
