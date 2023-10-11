161fd7cf38e239e6f91c3d128ddb8daa66cea127
Alan Liddell <aliddell@chanzuckerberg.com>
2023-10-11 14:50:43 -0400

    stream from Acquire/ROS

--- napari_ros/stream_viewer.py
+++ napari_ros/stream_viewer.py
@@ -3,9 +3,16 @@ from rclpy.qos import qos_profile_sensor_data
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 
-from cv_bridge import CvBridge
+from threading import Thread
+from typing import Optional
+import logging
+from multiprocessing import Process, Queue
 
-from napari.qt.threading import thread_worker  # type: ignore
+import napari
+import numpy as np
+
+queue = Queue()
+viewer: Optional[napari.Viewer] = None
 
 
 class NapariStreamViewer(Node):
@@ -20,18 +27,64 @@ class NapariStreamViewer(Node):
             ),
         )
 
-    def topic_callback(self, stream: int, msg: Image):
-        self.get_logger().info(f"Received image from stream {stream}.")
+    def update_layer(self, data, stream_id: int):
+        layer_key = f"Stream {stream_id}"
+        assert isinstance(data, np.ndarray)
+        queue.put((data, layer_key))
 
     def stream0(self, msg: Image):
-        self.topic_callback(0, msg)
+        self.get_logger().info("Got new frame on stream 0")
+        im = np.ndarray(
+            shape=(msg.height, msg.width),
+            dtype=np.uint8,
+            buffer=msg.data,
+        )
+        self.update_layer(im, 0)
 
     def stream1(self, msg: Image):
-        self.topic_callback(1, msg)
+        self.get_logger().info("Got new frame on stream 1")
+        im = np.ndarray(
+            shape=(msg.height, msg.width),
+            dtype=np.uint8,
+            buffer=msg.data,
+        )
+        self.update_layer(im, 1)
+
+
+def add_image(args):
+    global viewer
+    (data, layer_key) = args
+    try:
+        layer = viewer.layers[layer_key]
+        layer._slice.image._view = data
+        layer.events.set_data()
+    except KeyError:
+        viewer.add_image([data], name=layer_key)
+
+
+@napari.qt.thread_worker(connect={"yielded": add_image})
+def process_queue(q: Queue):
+    logging.info("starting thread worker")
+    while True:
+        logging.info("waiting")
+        v = q.get()
+        logging.info("Got data for ", v[1])
+        yield v
+
+
+def start_viewer(q):
+    global viewer
+    viewer = napari.Viewer()
+    worker = process_queue(q)
+    # worker.start()
+    napari.run()
 
 
 def main(args=None):
+    Process(target=start_viewer, args=(queue,), daemon=True).start()
+
     rclpy.init(args=args)
+
     napari_stream_viewer = NapariStreamViewer()
     rclpy.spin(napari_stream_viewer)
 
